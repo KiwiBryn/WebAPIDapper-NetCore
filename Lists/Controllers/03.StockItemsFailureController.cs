c809f203372a29943120097d91ba47951e66103a
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-06-16 12:02:11 +1200

    Added version with basic error handling using baked in logger plumbing. Moved stock Item DTO of to models folder

---
+++ Lists/Controllers/03.StockItemsFailureController.cs
@@ -0,0 +1,67 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) June 2021, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using Dapper;
+
+namespace devMobile.WebAPIDapper.Lists.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class StockItemsFailureController : ControllerBase
+	{
+		private readonly string connectionString;
+		private readonly ILogger<StockItemsFailureController> logger;
+
+		public StockItemsFailureController(IConfiguration configuration, ILogger<StockItemsFailureController> logger)
+		{
+			this.connectionString = configuration.GetSection("ConnectionStrings").GetSection("WideWorldImportersDatabase").Value;
+
+			this.logger = logger;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Model.StockItemListDtoV1>> Get()
+		{
+			IEnumerable<Model.StockItemListDtoV1> response = null;
+
+			try
+			{
+				using (SqlConnection db = new SqlConnection(this.connectionString))
+				{
+					response = db.Query<Model.StockItemListDtoV1>(sql: @"SELECT [StockItemID] as ""ID"", [StockItemName] as ""Name"", [RecommendedRetailPrice], [TaxRate] FROM [Warehouse].[StockItems]", commandType: CommandType.Text);
+				}
+			}
+			catch (SqlException ex)
+			{
+				logger.LogError(ex, "StockItemsFailure exception retrieving list of StockItems");
+
+				return this.StatusCode(StatusCodes.Status500InternalServerError);
+			}
+
+			return this.Ok(response);
+		}
+	}
+
+}
---
+++ Lists/Models/StockItem.cs
@@ -0,0 +1,81 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) June 2021, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace devMobile.WebAPIDapper.Lists.Model
+{
+	public class StockItemListDtoV1
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public decimal RecommendedRetailPrice { get; set; }
+
+		public decimal TaxRate { get; set; }
+	}
+
+	public class StockItemGetDtoV1
+	{
+		[JsonPropertyName("id")]
+		public int Id { get; set; }
+
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("unitPrice")]
+		public decimal UnitPrice { get; set; }
+
+		[JsonPropertyName("recommendedRetailPrice")]
+		public decimal RecommendedRetailPrice { get; set; }
+
+		[JsonPropertyName("taxRate")]
+		public decimal TaxRate { get; set; }
+
+		[JsonPropertyName("quantityPerOuter")]
+		public int QuantityPerOuter { get; set; }
+
+		[JsonPropertyName("typicalWeightPerUnit")]
+		public decimal TypicalWeightPerUnit { get; set; }
+
+		[JsonPropertyName("unitPackageName")]
+		public string UnitPackageName { get; set; }
+
+		[JsonPropertyName("outerPackageName")]
+		public string OuterPackageName { get; set; }
+
+		[JsonPropertyName("supplierID")]
+		public int SupplierID { get; set; }
+
+		[JsonPropertyName("supplierName")]
+		public string SupplierName { get; set; }
+
+	}
+
+	public class StockItemPagingDtoV1
+	{
+		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "PageSize must be present and greater than 0")]
+		public int PageSize { get; set; }
+
+		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "PageNumber must be present and greater than 0")]
+		public int PageNumber { get; set; }
+	}
+}
