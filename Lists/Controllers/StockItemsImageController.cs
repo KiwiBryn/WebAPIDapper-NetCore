001db034667ecbf5ff86138ea6fa8cc1f488c209
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-06-22 12:41:17 +1200

    Initial version og image controller with binay and base64 encoding

---
+++ Lists/Controllers/StockItemsImageController.cs
@@ -0,0 +1,101 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) June 2021, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using Dapper;
+
+
+namespace devMobile.WebAPIDapper.Lists.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StockItemsImageController : ControllerBase
+	{
+		private readonly string connectionString;
+		private readonly ILogger<StockItemsImageController> logger;
+
+		public StockItemsImageController(IConfiguration configuration, ILogger<StockItemsImageController> logger)
+		{
+			this.connectionString = configuration.GetSection("ConnectionStrings").GetSection("WideWorldImportersDatabase").Value;
+
+			this.logger = logger;
+		}
+
+		[HttpGet("{id}/jpg")]
+		public async Task<ActionResult<Task<FileStreamResult>>> Get([Range(1, int.MaxValue, ErrorMessage = "Stock item id must greater than 0")] int id)
+		{
+			Byte[] response;
+
+			try
+			{
+				using (SqlConnection db = new SqlConnection(this.connectionString))
+				{
+					response = await db.ExecuteScalarAsync<byte[]>(sql: @"SELECT [Photo] as ""photo"" FROM [WareHouse].[StockItems] WHERE StockItemID=@StockItemId", param: new { StockItemId = id }, commandType: CommandType.Text);
+				}
+
+				if (response == default)
+				{
+					return this.NotFound($"StockItem:{id} image not found");
+				}
+			}
+			catch (SqlException ex)
+			{
+				logger.LogError(ex, "StockItemsImage exception looking up a StockItem:{0}", id);
+
+				return this.StatusCode(StatusCodes.Status500InternalServerError);
+			}
+
+			return File(response, "image/jpeg");
+		}
+
+		[HttpGet("{id}/base64")]
+		public async Task<ActionResult> GetImage([Range(1, int.MaxValue, ErrorMessage = "Stock item id must greater than 0")] int id)
+		{
+			Byte[] response;
+
+			try
+			{
+				using (SqlConnection db = new SqlConnection(this.connectionString))
+				{
+					response = await db.ExecuteScalarAsync<byte[]>(sql: @"SELECT [Photo] as ""photo"" FROM [WareHouse].[StockItems] WHERE StockItemID=@StockItemId", param: new { StockItemId = id }, commandType: CommandType.Text);
+				}
+
+				if (response == default)
+				{
+					return this.NotFound($"StockItem:{id} image not found");
+				}
+			}
+			catch (SqlException ex)
+			{
+				logger.LogError(ex, "StockItemsImage exception looking up a StockItem:{0}", id);
+
+				return this.StatusCode(StatusCodes.Status500InternalServerError);
+			}
+
+			return Ok("data:image/jpeg;base64," + Convert.ToBase64String(response));
+		}
+	}
+}
