d325baa2a9debbf9ff59c937c49a66c8e28e69d3
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-06-22 18:12:33 +1200

    Added error catching code another GET method and fixed up un-necessary duplication in exception message

--- Lists/Controllers/StockGroupsVersionController.cs
+++ Lists/Controllers/StockGroupsVersionController.cs
@@ -14,13 +14,12 @@
 // limitations under the License.
 //
 //---------------------------------------------------------------------------------
-//ALTER TABLE[warehouse].[StockGroups]
-//ADD[Version] ROWVERSION NULL
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Http;
@@ -29,8 +28,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 using Dapper;
-using Microsoft.Net.Http.Headers;
-using System.Linq;
+
 
 namespace devMobile.WebAPIDapper.Lists.Controllers
 {
@@ -39,13 +37,51 @@ namespace devMobile.WebAPIDapper.Lists.Controllers
 	public class StockGroupsVersionController : ControllerBase
 	{
 		private readonly string connectionString;
-		private readonly ILogger<StockItemsImageController> logger;
+		private readonly ILogger<StockGroupsVersionController> logger;
 
-		public StockGroupsVersionController(IConfiguration configuration, ILogger<StockItemsImageController> logger)
+		// Would move these to a shared constants file
+		private const byte ETagBytesLength = 8;
+		private const byte ETagBase64Length = 12;
+
+		public StockGroupsVersionController(IConfiguration configuration, ILogger<StockGroupsVersionController> logger)
 		{
 			this.connectionString = configuration.GetSection("ConnectionStrings").GetSection("WideWorldImportersDatabase").Value;
 
 			this.logger = logger;
+		}
+
+		[HttpHead]
+		public async Task<ActionResult> Head([Required][FromHeader(Name = "ETag")][MinLength(ETagBase64Length, ErrorMessage = "eTag length invalid too short")][MaxLength(ETagBase64Length, ErrorMessage = "eTag length {0} invalid too long")] string eTag)
+		{
+			byte[] headerVersion = new byte[ETagBytesLength];
+
+			if (!Convert.TryFromBase64String(eTag, headerVersion, out _))
+			{
+				logger.LogInformation("eTag invalid format");
+
+				return this.BadRequest("eTag invalid format");
+			}
+
+			try
+			{
+				using (SqlConnection db = new SqlConnection(this.connectionString))
+				{
+					byte[] databaseVersion = await db.ExecuteScalarAsync<byte[]>(sql: "SELECT MAX(Version) FROM [Warehouse].[StockGroups]", commandType: CommandType.Text);
+
+					if (headerVersion.SequenceEqual(databaseVersion))
+					{
+						return this.StatusCode(StatusCodes.Status304NotModified);
+					}
+				}
+			}
+			catch (SqlException ex)
+			{
+				logger.LogError(ex, "Retrieving StockItem list");
+
+				return this.StatusCode(StatusCodes.Status500InternalServerError);
+			}
+
+			return this.Ok();
 		}
 
 		[HttpGet]
@@ -59,7 +95,7 @@ namespace devMobile.WebAPIDapper.Lists.Controllers
 				{
 					var parameters = new DynamicParameters();
 
-					parameters.Add("@RowVersion", dbType: DbType.Binary, direction: ParameterDirection.Output, size: 8);
+					parameters.Add("@RowVersion", dbType: DbType.Binary, direction: ParameterDirection.Output, size: ETagBytesLength);
 
 					response = await db.QueryAsync<Model.StockGroupListDtoV1>(sql: @"SELECT [StockGroupID] as ""ID"", [StockGroupName] as ""Name""FROM [Warehouse].[StockGroups] ORDER BY Name; SELECT @RowVersion=MAX(Version) FROM [Warehouse].[StockGroups]", param: parameters, commandType: CommandType.Text);
 
@@ -67,13 +103,13 @@ namespace devMobile.WebAPIDapper.Lists.Controllers
 					{
 						byte[] rowVersion = parameters.Get<byte[]>("RowVersion");
 
-						this.HttpContext.Response.Headers.Add(HeaderNames.ETag, Convert.ToBase64String(rowVersion));
+						this.HttpContext.Response.Headers.Add("ETag", Convert.ToBase64String(rowVersion));
 					}
 				}
 			}
 			catch (SqlException ex)
 			{
-				logger.LogError(ex, "StockGroupsVersion exception retrieving list of StockGroups and row version");
+				logger.LogError(ex, "Retrieving list of StockGroups");
 
 				return this.StatusCode(StatusCodes.Status500InternalServerError);
 			}
@@ -82,20 +118,29 @@ namespace devMobile.WebAPIDapper.Lists.Controllers
 		}
 
 		[HttpHead("{id}")]
-		public async Task<ActionResult> Head([Range(1, int.MaxValue, ErrorMessage = "Stock item id must greater than 0")] int id, [FromHeader(Name = "ETag")] string eTag)
+		public async Task<ActionResult> Head([Range(1, int.MaxValue, ErrorMessage = "Stock Group id must greater than 0")] int id, [Required][FromHeader(Name = "ETag")][MinLength(ETagBase64Length, ErrorMessage = "eTag length {0} invalid too short")][MaxLength(ETagBase64Length, ErrorMessage = "eTag length {0} invalid too long")] string eTag)
 		{
-			byte[] headerVersion = new byte[8];
-	
-			if (!Convert.TryFromBase64String(eTag, headerVersion, out int bytes) || (bytes != 8))
+			byte[] headerVersion = new byte[ETagBytesLength];
+
+			if (!Convert.TryFromBase64String(eTag, headerVersion, out _))
 			{
-				return this.BadRequest();
+				logger.LogInformation("eTag invalid format");
+
+				return this.BadRequest("eTag invalid format");
 			}
 
 			try
 			{
 				using (SqlConnection db = new SqlConnection(this.connectionString))
 				{
-					byte[] databaseVersion = await db.ExecuteScalarAsync<byte[]>(sql: "SELECT Version FROM [Warehouse].[StockGroups] WHERE [StockGroupID]=@id", param:new {id},commandType: CommandType.Text);
+					byte[] databaseVersion = await db.ExecuteScalarAsync<byte[]>(sql: "SELECT Version FROM [Warehouse].[StockGroups] WHERE [StockGroupID]=@id", param: new { id }, commandType: CommandType.Text);
+
+					if (databaseVersion==null)
+					{
+						logger.LogInformation("StockGroup:{0} not found", id);
+
+						return this.NotFound($"StockGroup:{id} not found");
+					}
 
 					if (headerVersion.SequenceEqual(databaseVersion))
 					{
@@ -105,7 +150,7 @@ namespace devMobile.WebAPIDapper.Lists.Controllers
 			}
 			catch (SqlException ex)
 			{
-				logger.LogError(ex, "StockGroupsVersion exception retrieving row version");
+				logger.LogError(ex, "Retrieving rowversion for StockGroup:{0}", id);
 
 				return this.StatusCode(StatusCodes.Status500InternalServerError);
 			}
@@ -114,7 +159,7 @@ namespace devMobile.WebAPIDapper.Lists.Controllers
 		}
 
 		[HttpGet("{id}")]
-		public async Task<ActionResult<Model.StockGroupGetDtoV1>> Get([Range(1, int.MaxValue, ErrorMessage = "Stock item id must greater than 0")] int id)
+		public async Task<ActionResult<Model.StockGroupGetDtoV1>> Get([Range(1, int.MaxValue, ErrorMessage = "Stock Group id must greater than 0")] int id)
 		{
 			Model.StockGroupGetDtoV1 response = null;
 
@@ -122,17 +167,21 @@ namespace devMobile.WebAPIDapper.Lists.Controllers
 			{
 				using (SqlConnection db = new SqlConnection(this.connectionString))
 				{
-					response = await db.QuerySingleOrDefaultAsync<Model.StockGroupGetDtoV1>(sql: @"SELECT [StockGroupID] as ""ID"", [Version], [StockGroupName] as ""Name"" FROM [Warehouse].[StockGroups] WHERE [StockGroupID]=@id", param:new{id}, commandType: CommandType.Text);
+					response = await db.QuerySingleOrDefaultAsync<Model.StockGroupGetDtoV1>(sql: @"SELECT [StockGroupID] as ""ID"", [Version], [StockGroupName] as ""Name"" FROM [Warehouse].[StockGroups] WHERE [StockGroupID]=@id", param: new { id }, commandType: CommandType.Text);
 
-					if (response!=null)
+					if (response == null)
 					{
-						this.HttpContext.Response.Headers.Add(HeaderNames.ETag, Convert.ToBase64String(response.Version));
+						logger.LogInformation("StockItem:{0} not found", id);
+
+						return this.NotFound($"StockItem:{id} not found");
 					}
+
+					this.HttpContext.Response.Headers.Add("ETag", Convert.ToBase64String(response.Version));
 				}
 			}
 			catch (SqlException ex)
 			{
-				logger.LogError(ex, "StockItemsAsync exception retrieving list of StockItems");
+				logger.LogError(ex, "Error retrieving StockGroup:{0}", id);
 
 				return this.StatusCode(StatusCodes.Status500InternalServerError);
 			}
