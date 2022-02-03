ac6abde6a1671384d148cb785297b0870f3f3e1d
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-02-03 23:11:48 +1300

    First version of caching samples with ResponseCache

---
+++ Lists/Controllers/10.StockItemsCachingController.cs
@@ -0,0 +1,107 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) Feb 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using devMobile.Azure.DapperTransient;
+
+
+namespace devMobile.WebAPIDapper.Lists.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StockItemsCachingController : ControllerBase
+	{
+		private const int StockItemsListResponseCacheDuration = 30;
+		private readonly string connectionString;
+		private readonly ILogger<StockItemsCachingController> logger;
+
+		public StockItemsCachingController(IConfiguration configuration, ILogger<StockItemsCachingController> logger)
+		{
+			this.connectionString = configuration.GetConnectionString("WorldWideImportersDatabase");
+
+			this.logger = logger;
+		}
+
+		[HttpGet("Response")]
+		[ResponseCache(Duration = StockItemsListResponseCacheDuration)]
+		public async Task<ActionResult<IAsyncEnumerable<Model.StockItemListDtoV1>>> GetResponse()
+		{
+			IEnumerable<Model.StockItemListDtoV1> response = null;
+
+			logger.LogInformation("Response cache load");
+
+			try
+			{
+				using (SqlConnection db = new SqlConnection(this.connectionString))
+				{
+					response = await db.QueryWithRetryAsync<Model.StockItemListDtoV1>(sql: @"SELECT [StockItemID] as ""ID"", [StockItemName] as ""Name"", [RecommendedRetailPrice], [TaxRate] FROM [Warehouse].[StockItems]", commandType: CommandType.Text);
+				}
+			}
+			catch (SqlException ex)
+			{
+				logger.LogError(ex, "Retrieving list of StockItems");
+
+				return this.StatusCode(StatusCodes.Status500InternalServerError);
+			}
+
+			return this.Ok(response);
+		}
+
+		[HttpGet("ResponseVarying")]
+		[ResponseCache(Duration = StockItemsListResponseCacheDuration, VaryByQueryKeys = new string[] { "id" })]
+		public async Task<ActionResult<Model.StockItemGetDtoV1>> Get([FromQuery(Name = "id"), Range(1, int.MaxValue, ErrorMessage = "Stock item id must greater than 0")] int id)
+		{
+			Model.StockItemGetDtoV1 response = null;
+
+			logger.LogInformation("Response cache varying load id:{0}", id);
+
+			try
+			{
+				using (SqlConnection db = new SqlConnection(this.connectionString))
+				{
+					response = await db.QuerySingleOrDefaultWithRetryAsync<Model.StockItemGetDtoV1>(sql: "[Warehouse].[StockItemsStockItemLookupV1]", param: new { stockItemId = id }, commandType: CommandType.StoredProcedure);
+				}
+
+				if (response == default)
+				{
+					logger.LogInformation("StockItem:{0} not found", id);
+
+					return this.NotFound($"StockItem:{id} not found");
+				}
+			}
+			catch (SqlException ex)
+			{
+				logger.LogError(ex, "Looking up StockItem with Id:{0}", id);
+
+				return this.StatusCode(StatusCodes.Status500InternalServerError);
+			}
+
+			return this.Ok(response);
+		}
+	}
+}
+
