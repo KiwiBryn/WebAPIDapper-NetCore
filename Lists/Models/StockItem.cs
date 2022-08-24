447f1a9a1bf8bac3273260f53de7b3caa6ebcebc
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-08-24 12:15:32 +1200

    Tidy up attributes

--- Lists/Models/StockItem.cs
+++ Lists/Models/StockItem.cs
@@ -16,102 +16,102 @@
 //---------------------------------------------------------------------------------
 namespace devMobile.WebAPIDapper.Lists.Model
 {
-	using System;
-	using System.ComponentModel.DataAnnotations;
+   using System;
+   using System.ComponentModel.DataAnnotations;
 
-    public class StockItemListDtoV1
-	{
-		public int Id { get; set; }
+   public class StockItemListDtoV1
+   {
+      public int Id { get; set; }
 
-		public string Name { get; set; }
+      public string Name { get; set; }
 
-		public decimal RecommendedRetailPrice { get; set; }
+      public decimal RecommendedRetailPrice { get; set; }
 
-		public decimal TaxRate { get; set; }
-	}
+      public decimal TaxRate { get; set; }
+   }
 
-	public class StockItemGetDtoV1
-	{
-		public int Id { get; set; }
+   public class StockItemGetDtoV1
+   {
+      public int Id { get; set; }
 
-		public string Name { get; set; }
+      public string Name { get; set; }
 
-		public decimal UnitPrice { get; set; }
+      public decimal UnitPrice { get; set; }
 
-		public decimal RecommendedRetailPrice { get; set; }
+      public decimal RecommendedRetailPrice { get; set; }
 
-		public decimal TaxRate { get; set; }
+      public decimal TaxRate { get; set; }
 
-		public int QuantityPerOuter { get; set; }
+      public int QuantityPerOuter { get; set; }
 
-		public decimal TypicalWeightPerUnit { get; set; }
+      public decimal TypicalWeightPerUnit { get; set; }
 
-		public string UnitPackageName { get; set; }
+      public string UnitPackageName { get; set; }
 
-		public string OuterPackageName { get; set; }
+      public string OuterPackageName { get; set; }
 
-		public int SupplierID { get; set; }
+      public int SupplierID { get; set; }
 
-		public string SupplierName { get; set; }
+      public string SupplierName { get; set; }
+   }
 
-	}
+   public class StockItemPagingDtoV1
+   {
+      [Required]
+      [Range(1, int.MaxValue, ErrorMessage = "PageSize must be present and greater than {1}")]
+      public int PageSize { get; set; }
 
-	public class StockItemPagingDtoV1
-	{
-		[Required]
-		[Range(1, int.MaxValue, ErrorMessage = "PageSize must be present and greater than {1}")]
-		public int PageSize { get; set; }
+      [Required]
+      [Range(1, int.MaxValue, ErrorMessage = "PageNumber must be present and greater than {1}")]
+      public int PageNumber { get; set; }
+   }
 
-		[Required]
-		[Range(1, int.MaxValue, ErrorMessage = "PageNumber must be present and greater than {1}")]
-		public int PageNumber { get; set; }
-	}
+   public class StockItemNameSearchDtoV1
+   {
+      [Required]
+      [MinLength(3, ErrorMessage = "The name search text must be at least {1} characters long"), MaxLength(20, ErrorMessage = "The name search text must be no more that {1} characters long")]
+      public string SearchText { get; set; }
 
-	public class StockItemNameSearchDtoV1
-	{
-        [Required]
-        [MinLength(3, ErrorMessage = "The name search text must be at least {1} characters long"), MaxLength(20, ErrorMessage = "The name search text must be no more that {1} characters long")]
-        public string SearchText { get; set; }
+      [Required]
+      [Range(1, int.MaxValue, ErrorMessage = "MaximumRowsToReturn must be at least {1}")]
+      public int MaximumRowsToReturn { get; set; }
+   }
 
-        [Required][Range(1, int.MaxValue, ErrorMessage = "MaximumRowsToReturn must be at least {1}")]
-        public int MaximumRowsToReturn { get; set; }
-	}
+   public class StockItemsHistoryListDtoV1
+   {
+      public int Id { get; set; }
 
-	public class StockItemsHistoryListDtoV1
-	{
-		public int Id { get; set; }
+      public string Name { get; set; }
 
-		public string Name { get; set; }
+      public decimal UnitPrice { get; set; }
 
-		public decimal UnitPrice { get; set; }
+      public decimal RecommendedRetailPrice { get; set; }
 
-		public decimal RecommendedRetailPrice { get; set; }
+      public decimal TaxRate { get; set; }
 
-		public decimal TaxRate { get; set; }
+      public string CustomFields { get; set; }
 
-		public string CustomFields { get; set; }
+      public int SupplierID { get; set; }
 
-		public int SupplierID { get; set; }
+      public string SupplierName { get; set; }
+   }
 
-		public string SupplierName { get; set; }
-	}
+   public class StockItemHistoryListDtoV1
+   {
+      public int Id { get; set; }
 
-	public class StockItemHistoryListDtoV1
-	{
-		public int Id { get; set; }
+      public string Name { get; set; }
 
-		public string Name { get; set; }
+      public decimal UnitPrice { get; set; }
 
-		public decimal UnitPrice { get; set; }
+      public decimal RecommendedRetailPrice { get; set; }
 
-		public decimal RecommendedRetailPrice { get; set; }
+      public decimal TaxRate { get; set; }
 
-		public decimal TaxRate { get; set; }
+      public string CustomFields { get; set; }
 
-		public string CustomFields { get; set; }
+      public DateTime ValidFrom { get; set; }
 
-		public DateTime ValidFrom { get; set; }
-
-		public DateTime ValidTo { get; set; }
-	}
+      public DateTime ValidTo { get; set; }
+   }
 }
