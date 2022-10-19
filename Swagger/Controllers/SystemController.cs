e878b75f85c5be5a2a90e9c1a607686b6ea94fa3
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-10-20 12:29:58 +1300

    Added System controller whihc displays WebAPI version information

---
+++ Swagger/Controllers/SystemController.cs
@@ -0,0 +1,33 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) October 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace Swagger.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        [HttpGet]
+        public string DeploymentVersion()
+        {
+            // https://marinovdh.wordpress.com/2018/10/22/68/
+            //
+            return this.GetType().Assembly.GetName().Version.ToString();
+        }
+    }
+}
