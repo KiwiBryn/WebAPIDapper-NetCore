9dc384d23c18caa3dbe6c075325366c6c66bc84e
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-11-02 19:52:28 +1300

    Added start if Warehouse support for picking

---
+++ AuthenticationAndAuthorisationJWTDatabase/Controllers/WharehouseController.cs
@@ -0,0 +1,99 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) October 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.WebAPIDapper.AuthenticationAndAuthorisationJwtDatabase.Controllers
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    using devMobile.Azure.DapperTransient;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// WebAPI controller for handling Invoice functionality.
+    /// </summary>
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class WarehouseController : ControllerBase
+    {
+        private readonly string connectionString;
+        private readonly ILogger<WarehouseController> logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvoiceController"/> class.
+        /// </summary>
+        /// <param name="configuration">DI configuration provider.</param>
+        /// <param name="logger">DI logging provider.</param>/// 
+        public WarehouseController(IConfiguration configuration, ILogger<WarehouseController> logger)
+        {
+            this.connectionString = configuration.GetConnectionString("WorldWideImportersDatabase");
+
+            this.logger = logger;
+        }
+
+        [Authorize(Roles = "WarehousePerson,WarehouseAdministrator,Administrator")]
+        [HttpGet("NextToPick")]
+        public async Task<ActionResult<IAsyncEnumerable<Models.OrderToPickListDtoV1>>> Get()
+        {
+            IEnumerable<Models.OrderToPickListDtoV1> response;
+
+            using (SqlConnection db = new SqlConnection(this.connectionString))
+            {
+                response = await db.QueryWithRetryAsync<Models.OrderToPickListDtoV1>(sql: "[Warehouse].[OrderNextReadyToPickV1]", param:new { UserId = HttpContext.PersonId()}, commandType: CommandType.StoredProcedure);
+            }
+
+            if (!response.Any())
+            {
+                logger.LogInformation("No orders ready to pick");
+            }
+
+            return this.Ok(response);
+        }
+
+        [Authorize(Roles = "WarehousePerson,WarehouseAdministrator,Administrator")]
+        [HttpGet("OrderToPick")]
+        public async Task<ActionResult<Models.OrderToPickGetDtoV1>> Get([Required][Range(1, int.MaxValue, ErrorMessage = "Order id must greater than 0")] int orderId)
+        {
+            Models.OrderToPickGetDtoV1 response;
+
+            using (SqlConnection db = new SqlConnection(this.connectionString))
+            {
+                var invoiceSummary = await db.QueryMultipleWithRetryAsync("[Sales].[InvoiceSummaryGetV1]", commandType: CommandType.StoredProcedure);
+
+                response = await invoiceSummary.ReadSingleOrDefaultWithRetryAsync<Models.InvoiceSummaryGetDtoV1>();
+                if (response == default)
+                {
+                    logger.LogInformation("Order:{invoiceId} not found", orderId);
+
+                    return this.NotFound($"Invoice:{orderId} not found");
+                }
+
+                response.OrderLinesToPick = (await invoiceSummary.ReadWithRetryAsync<Models.OrderLineToPickListDtoV1>()).ToArray();
+            }
+
+            return this.Ok(response);
+        }
+    }
+}
---
+++ AuthenticationAndAuthorisationJWTDatabase/Models/Order.cs
@@ -0,0 +1,119 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) November 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.WebAPIDapper.AuthenticationAndAuthorisationJwtDatabase.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using Newtonsoft.Json;
+
+    public class OrderToPickListDtoV1
+    {
+        /// <summary>
+        /// Numeric ID used for reference to a order within the database
+        /// </summary>
+        public int OrderId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public int SalesPersonPersonId { get; set; }
+
+        public string SalesPersonName { get; set; }
+
+        public string CustomerPurchaseOrderNumber { get; set; } 
+
+        public DateTime ExpectedDeliveryDate { get; set; }
+    }
+
+    /// <summary>
+    /// DTO for returning summarised list of stock item information.
+    /// </summary>
+    public class OrderToPickGetDtoV1
+    {
+        /// <summary>
+        /// Numeric ID used for reference to a order within the database
+        /// </summary>
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public int SalesPersonId { get; set; }
+
+        public string SalesPersonName { get; set; }
+
+        public string DeliveryAddressLine1 { get; set; }
+
+        public string DeliveryAddressLine2 { get; set; }
+
+        public string DeliveryPostalCode { get; set; }
+
+        public string DeliveryCity { get; set; }
+
+        public string DeliveryLocation { get; set; }
+
+        public string DeliveryRun { get; set; }
+
+        public string DeliveryRunPosition { get; set; }
+
+        public bool IsOneCreditHold { get; set; }
+
+        public OrderLineToPickListDtoV1[] OrderLinesToPick { get; set; }
+    }
+
+    public class OrderLineToPickListDtoV1
+    {
+        public int Id { get; set; }
+
+        public int StockItemId { get; set; }
+
+        public string StockItemName { get; set; }
+
+        public int StockItemColourId { get; set; }
+
+        public string StockItemColourName { get; set; }
+
+        public int StockItemUnitPackageId { get; set; }
+
+        public string StockItemUnitPackageName { get; set; }
+
+        public int StockItemOuterPackageId { get; set; }
+
+        public string StockItemOuterPackageName { get; set; }
+
+        public string StockItemBrand { get; set; }
+
+        public string StockItemSize{ get; set; }
+
+        public string StockQuantityPerOuter { get; set; }
+
+        public bool IsChillerStock{ get; set; }
+
+        public string StockItemBarcode { get; set; }
+
+        public double TypicalWeightPerUnit { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int QuantityPicked { get; set; }
+
+        public DateTime PickingCompletedAt { get; set; }
+    }
+}
