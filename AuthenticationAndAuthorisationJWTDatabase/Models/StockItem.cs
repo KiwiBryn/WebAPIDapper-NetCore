7fa12bc25ef5cf86753b1293361bb8afad987ec5
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-11-02 19:44:19 +1300

    Added JSON ignore decoration to UserID so not featured in Swagger UI

--- AuthenticationAndAuthorisationJWTDatabase/Models/StockItem.cs
+++ AuthenticationAndAuthorisationJWTDatabase/Models/StockItem.cs
@@ -19,6 +19,8 @@ namespace devMobile.WebAPIDapper.AuthenticationAndAuthorisationJwtDatabase.Model
     using System;
     using System.ComponentModel.DataAnnotations;
 
+    using Newtonsoft.Json;
+
     /// <summary>
     /// DTO for returning summarised list of stock item information.
     /// </summary>
@@ -110,6 +112,9 @@ namespace devMobile.WebAPIDapper.AuthenticationAndAuthorisationJwtDatabase.Model
     /// </summary>
     public class StockItemNameSearchDtoV1
     {
+        [JsonIgnore]
+        public int UserId { get; set; }
+
         /// <summary>
         /// Text in stock item name to search for.
         /// </summary>
