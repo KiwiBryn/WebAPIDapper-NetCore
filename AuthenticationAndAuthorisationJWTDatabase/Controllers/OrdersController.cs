32b221a91d1b5768bd04914a811467313fb5d68b
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-11-02 19:42:59 +1300

    Stubbed out orders controller

---
+++ AuthenticationAndAuthorisationJWTDatabase/Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) November 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.WebAPIDapper.AuthenticationAndAuthorisationJwtDatabase.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly string connectionString;
+        private readonly ILogger<OrdersController> logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrdersController"/> class.
+        /// </summary>
+        /// <param name="configuration">DI configuration provider.</param>
+        /// <param name="logger">DI logging provider.</param>/// 
+        public OrdersController(IConfiguration configuration, ILogger<OrdersController> logger)
+        {
+            this.connectionString = configuration.GetConnectionString("WorldWideImportersDatabase");
+
+            this.logger = logger;
+        }
+    }
+}
