2bc88fac862d52d1421cef0c0e509815861a6370
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-07-24 10:37:57 +1200

    Initiial version of ASP Net core website SendGrid password reset working with natsy configuration Nasmespace needs fixing up

---
+++ WebAppCoreIdentity/Services/SendGridEmailSender.cs
@@ -0,0 +1,49 @@
+﻿// https://www.twilio.com/blog/how-to-send-asp-net-core-identity-emails-with-twilio-sendgrid
+using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace WebAppIdentity;
+
+public class SendGridEmailSender : IEmailSender
+{
+    private readonly IConfiguration configuration;
+    private readonly ILogger logger;
+
+    public SendGridEmailSender(IConfiguration configuration, ILogger<SendGridEmailSender> logger)
+    {
+        this.configuration = configuration;
+        this.logger = logger;
+    }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string message)
+    {
+        string sendGridApiKey = configuration["SendGridApiKey"];
+        if (string.IsNullOrEmpty(sendGridApiKey))
+        {
+            throw new Exception("The 'SendGridApiKey' is not configured");
+        }
+
+        var client = new SendGridClient(sendGridApiKey);
+        var msg = new SendGridMessage()
+        {
+            From = new EmailAddress(configuration["EmailAddressFrom"], configuration["WebSiteFrom"]),
+            Subject = subject,
+            PlainTextContent = message,
+            HtmlContent = message
+        };
+        msg.AddTo(new EmailAddress(toEmail));
+
+        var response = await client.SendEmailAsync(msg);
+        if (response.IsSuccessStatusCode)
+        {
+            logger.LogInformation("Email queued successfully");
+        }
+        else
+        {
+            logger.LogError("Failed to send email");
+            // Adding more information related to the failed email could be helpful in debugging failure,
+            // but be careful about logging PII, as it increases the chance of leaking PII
+        }
+    }
+}
