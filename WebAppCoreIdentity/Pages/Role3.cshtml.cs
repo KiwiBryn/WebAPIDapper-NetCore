0b131b01406f795bfa54febc51d30eddfd250e6d
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-07-24 15:26:14 +1200

    Added pages for checking roles

---
+++ WebAppCoreIdentity/Pages/Role3.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace devMobile.WebAPIDapper.WebAppCoreIdentity.Pages
+{
+    [Authorize(Roles = "Role3")]
+    public class Role3Model : PageModel
+    {
+        private readonly ILogger<Role3Model> _logger;
+
+        public Role3Model(ILogger<Role3Model> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
