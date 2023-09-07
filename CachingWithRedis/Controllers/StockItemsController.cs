adc27b4bd229966911d68cf6c37170cf06a2fd31
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-09-07 14:36:10 +1200

    First basic working version of caching with Redis native API

---
+++ CachingWithRedis/Controllers/StockItemsController.cs
@@ -0,0 +1,226 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) September 2023, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+//#define SERIALISATION_JSON or SERIALISATION_MESSAGE_PACK
+#if SERIALISATION_JSON && SERIALISATION_MESSAGE_PACK
+    #error Only one serialisation method can be defined
+#endif
+#if !SERIALISATION_JSON && !SERIALISATION_MESSAGE_PACK
+    #error At most one serialisation method can be defined
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+#if SERIALISATION_JSON
+    using System.Text.Json;
+#endif
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+#if SERIALISATION_MESSAGE_PACK 
+    using MessagePack;
+#endif
+
+using devMobile.Azure.DapperTransient;
+using devMobile.Dapper;
+
+using StackExchange.Redis;
+using System.ComponentModel.DataAnnotations;
+
+namespace devMobile.WebAPIDapper.CachingWithRedis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StockItemsController : ControllerBase
+    {
+        private const int StockItemSearchMaximumRowsToReturn = 15;
+        //private readonly TimeSpan StockItemListAbsoluteExpiration = new TimeSpan(23, 59, 59);
+        //private readonly TimeSpan StockItemsSearchSlidingExpiration = new TimeSpan(0, 1, 0);
+
+        private const string sqlCommandText = @"SELECT [StockItemID] as ""ID"", [StockItemName] as ""Name"", [RecommendedRetailPrice], [TaxRate] FROM [Warehouse].[StockItems]";
+        //private const string sqlCommandText = @"SELECT [StockItemID] as ""ID"", [StockItemName] as ""Name"", [RecommendedRetailPrice], [TaxRate] FROM [Warehouse].[StockItems]; WAITFOR DELAY '00:00:02'";
+
+        private readonly ILogger<StockItemsController> logger;
+        private readonly IDbConnection dbConnection;
+        private readonly IDatabase redisCache;
+
+        public StockItemsController(ILogger<StockItemsController> logger, IDapperContext dapperContext, IDatabase redisCache)
+        {
+            this.logger = logger;
+
+            this.dbConnection = dapperContext.ConnectionCreate();
+
+            this.redisCache = redisCache;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Model.StockItemListDtoV1>>> Get()
+        {
+            var utcNow = DateTime.UtcNow;
+
+            var cached = await redisCache.StringGetAsync("StockItems");
+            if (cached.HasValue)
+            {
+#if SERIALISATION_JSON
+                return this.Ok(JsonSerializer.Deserialize<List<Model.StockItemListDtoV1>>(cached));
+#endif
+#if SERIALISATION_MESSAGE_PACK
+                return this.Ok(MessagePackSerializer.Deserialize<List<Model.StockItemListDtoV1>>(cached));
+#endif
+            }
+
+            var stockItems = await dbConnection.QueryWithRetryAsync<Model.StockItemListDtoV1>(sql: sqlCommandText, commandType: CommandType.Text);
+
+#if SERIALISATION_JSON
+            await redisCache.StringGetSetAsync("StockItems", JsonSerializer.SerializeToUtf8Bytes(stockItems));
+#endif
+#if SERIALISATION_MESSAGE_PACK
+            await redisCache.SetAddAsync("StockItems", MessagePackSerializer.Serialize(stockItems));
+#endif
+//            {
+//                AbsoluteExpiration = new DateTime(utcNow.Year, utcNow.Month, DateTime.DaysInMonth(utcNow.Year, utcNow.Month), StockItemListAbsoluteExpiration.Hours, StockItemListAbsoluteExpiration.Minutes, StockItemListAbsoluteExpiration.Seconds)
+//            });
+
+            return this.Ok(stockItems);
+        }
+
+        [HttpGet("NoLoad")]
+        public async Task<ActionResult<IEnumerable<Model.StockItemListDtoV1>>> GetNoLoad()
+        {
+            var cached = await redisCache.StringGetAsync("StockItems");
+            if (!cached.HasValue)
+            {
+                return this.NoContent();
+            }
+
+#if SERIALISATION_JSON
+            return this.Ok(JsonSerializer.Deserialize<List<Model.StockItemListDtoV1>>(cached));
+#endif
+#if SERIALISATION_MESSAGE_PACK
+             return this.Ok(MessagePackSerializer.Deserialize<List<Model.StockItemListDtoV1>>(cached));
+#endif
+        }
+
+
+        [HttpPost("Load")]
+        public async Task<ActionResult<IEnumerable<Model.StockItemListDtoV1>>> PostLoad()
+        {
+            var utcNow = DateTime.UtcNow;
+
+            var stockItems = await dbConnection.QueryWithRetryAsync<Model.StockItemListDtoV1>(sql: sqlCommandText, commandType: CommandType.Text);
+
+#if SERIALISATION_JSON
+            await redisCache.SetAddAsync("StockItems", JsonSerializer.SerializeToUtf8Bytes(stockItems));
+#endif
+#if SERIALISATION_MESSAGE_PACK
+            await redisCache.SetAddAsync("StockItems", MessagePackSerializer.Serialize(stockItems));
+#endif
+//            {
+//                AbsoluteExpiration = new DateTime(utcNow.Year, utcNow.Month, DateTime.DaysInMonth(utcNow.Year, utcNow.Month), StockItemListAbsoluteExpiration.Hours, StockItemListAbsoluteExpiration.Minutes, StockItemListAbsoluteExpiration.Seconds)
+//            });
+
+            return this.Ok();
+        }
+
+        [HttpDelete()]
+        public async Task<ActionResult> ListCacheDelete()
+        {
+            await redisCache.KeyDeleteAsync("StockItems");
+
+            logger.LogInformation("StockItems list removed");
+
+            return this.Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Model.StockItemGetDtoV1>> Get(int id)
+        {
+            var cached = await redisCache.StringGetAsync($"StockItem:{id}");
+            if (!cached.HasValue)
+            {
+#if SERIALISATION_JSON
+                return this.Ok(JsonSerializer.Deserialize<Model.StockItemGetDtoV1>(cached));
+#endif
+#if SERIALISATION_MESSAGE_PACK
+                return this.Ok(MessagePackSerializer.Deserialize<Model.StockItemGetDtoV1>(cached));
+#endif
+            }
+
+            var stockItem = await dbConnection.QuerySingleOrDefaultWithRetryAsync<Model.StockItemGetDtoV1>(sql: "[Warehouse].[StockItemsStockItemLookupV1]", param: new { stockItemId = id }, commandType: CommandType.StoredProcedure);
+            if (stockItem == default)
+            {
+                logger.LogInformation("StockItem:{id} not found", id);
+
+                return this.NotFound($"StockItem:{id} not found");
+            }
+
+#if SERIALISATION_JSON
+            await redisCache.SetAddAsync($"StockItem:{id}", JsonSerializer.SerializeToUtf8Bytes<Model.StockItemGetDtoV1>(stockItem)); ///, new DistributedCacheEntryOptions()
+#endif
+#if SERIALISATION_MESSAGE_PACK
+            await distributedCache.SetAsync($"StockItem:{id}", MessagePackSerializer.Serialize<Model.StockItemGetDtoV1>(stockItem), new DistributedCacheEntryOptions()
+#endif
+///            {
+///                SlidingExpiration = StockItemsSearchSlidingExpiration
+///            });
+
+            return this.Ok(stockItem);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult ItemCacheDelete(int id)
+        {
+            redisCache.KeyDelete($"StockItem:{id}");
+
+            logger.LogInformation("StockItem:{id} removed", id);
+
+            return this.NoContent();
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Model.StockItemListDtoV1>>> Get([Required][MinLength(3, ErrorMessage = "The name search text must be at least 3 characters long")] string searchText)
+        {
+            var cached = await redisCache.StringGetAsync($"StockItemsSearch:{searchText.ToLower()}");
+            if (!cached.HasValue)
+            {
+#if SERIALISATION_JSON
+                return this.Ok(JsonSerializer.Deserialize<List<Model.StockItemListDtoV1>>(cached));
+#endif
+#if SERIALISATION_MESSAGE_PACK
+                return this.Ok(MessagePackSerializer.Deserialize<List<Model.StockItemListDtoV1>>(cached));
+#endif
+            }
+
+            var stockItems = await dbConnection.QueryWithRetryAsync<Model.StockItemListDtoV1>(sql: "[Warehouse].[StockItemsNameSearchV1]", param: new { searchText, MaximumRowsToReturn = StockItemSearchMaximumRowsToReturn }, commandType: CommandType.StoredProcedure);
+
+#if SERIALISATION_JSON
+            await redisCache.StringSetAsync($"StockItemsSearch:{searchText.ToLower()}", JsonSerializer.SerializeToUtf8Bytes(stockItems));//, new DistributedCacheEntryOptions()
+#endif
+#if SERIALISATION_MESSAGE_PACK
+            await distributedCache.SetAsync($"StockItemsSearch:{searchText.ToLower()}", MessagePackSerializer.Serialize(stockItems), new DistributedCacheEntryOptions()
+#endif
+//            {
+//                SlidingExpiration = StockItemsSearchSlidingExpiration
+//            });
+
+            return this.Ok(stockItems);
+        }
+    }
+}
+
--- CachingWithRedis/Program.cs
+++ CachingWithRedis/Program.cs
@@ -1,4 +1,31 @@
-namespace CachingWithRedis
+//---------------------------------------------------------------------------------
+// Copyright (c) September 2023, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// https://localhost:7054/profiler/results-index
+//---------------------------------------------------------------------------------
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using MessagePack;
+using StackExchange.Redis;
+
+using devMobile.Dapper;
+
+
+namespace devMobile.WebAPIDapper.CachingWithRedis
 {
     public class Program
     {
@@ -7,8 +34,33 @@ namespace CachingWithRedis
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddApplicationInsightsTelemetry();
+
+            // Add services to the container.
+            builder.Services.AddSingleton<IDapperContext>(s => new DapperContext(builder.Configuration));
 
             builder.Services.AddControllers();
+
+#if SERIALISATION_MESSAGE_PACK
+            //MessagePackSerializer.DefaultOptions = MessagePack.Resolvers.ContractlessStandardResolver.Options;
+            //MessagePackSerializer.DefaultOptions = MessagePack.Resolvers.ContractlessStandardResolver.Options.WithCompression(MessagePackCompression.Lz4Block);
+            MessagePackSerializer.DefaultOptions = MessagePack.Resolvers.ContractlessStandardResolver.Options.WithCompression(MessagePackCompression.Lz4BlockArray);
+#endif
+            var configurationOptions = new ConfigurationOptions
+            {
+                EndPoints = { builder.Configuration.GetSection("RedisConnection").GetValue<string>("EndPoints") },
+                AllowAdmin = true,
+                Password = builder.Configuration.GetSection("RedisConnection").GetValue<string>("Password"),
+                Ssl = true,
+                ConnectRetry = 5,
+                ConnectTimeout = 10000,
+                SslProtocols = System.Security.Authentication.SslProtocols.Tls12,
+                AbortOnConnectFail = false,
+            };
+
+            var redis = ConnectionMultiplexer.Connect(configurationOptions);
+
+            builder.Services.AddSingleton<IDatabase>(s => redis.GetDatabase());
 
             var app = builder.Build();
 
@@ -17,8 +69,6 @@ namespace CachingWithRedis
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
