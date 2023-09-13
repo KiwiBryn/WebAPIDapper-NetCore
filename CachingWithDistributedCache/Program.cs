2499a25d2c3ac441fd464ff672a2b3b91ce51a45
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-09-13 17:57:36 +1200

    Got rid of hangover copy n paste

--- CachingWithDistributedCache/Program.cs
+++ CachingWithDistributedCache/Program.cs
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// https://localhost:7054/profiler/results-index
 //---------------------------------------------------------------------------------
 using Microsoft.AspNetCore.Builder;
 #if DISTRIBUTED_CACHE_REDIS || DISTRIBUTED_CACHE_SQL_SERVER
