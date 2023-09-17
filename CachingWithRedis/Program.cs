b6c6abebe03c6d64d9e0c499cb36a28f4845c357
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-09-17 15:07:13 +1200

    Updated the shortcut startup URL

--- CachingWithRedis/Program.cs
+++ CachingWithRedis/Program.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// https://localhost:7054/profiler/results-index
+// https://localhost:7059/api/stockitems
 //---------------------------------------------------------------------------------
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
